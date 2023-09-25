c35ea1cef95922ddc880aabbf9680d3932beb06c
Gio Saray <110672506+GSaraySWO@users.noreply.github.com>
2023-09-25 11:50:24 -0500

    Se adicionó funcionalidad x

--- src/Tests/Services/Application.FunctionalTests/Extensions/HttpClientExtensions.cs
+++ src/Tests/Services/Application.FunctionalTests/Extensions/HttpClientExtensions.cs
@@ -4,6 +4,7 @@ static class HttpClientExtensions
 {
     public static HttpClient CreateIdempotentClient(this TestServer server)
     {
+        //demo comentario
         var client = server.CreateClient();
         client.DefaultRequestHeaders.Add("x-requestid", Guid.NewGuid().ToString());
         return client;
